b4f7247acd9fd5e1e4e66dc22485f11d241f0e38
Vaarush-Technologies <hrvaarush@gmail.com>
2024-12-13 16:25:09 +0530

    Jayachandra

---
+++ Jaya Chnandra portfolio/scirpt.js
@@ -0,0 +1,4 @@
+document.querySelector('.btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+  });
